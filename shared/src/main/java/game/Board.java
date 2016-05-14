8d02f6d28288f49a6cbd1002dfd65e05343b9b41
goeckeler <thorsten@goeckeler.com>
2016-05-14 14:40:28 +0200

    Added starting player to board.

--- shared/src/main/java/game/Board.java
+++ shared/src/main/java/game/Board.java
@@ -1,47 +1,55 @@
 package game;
 
-import com.google.common.base.Preconditions;
+import static com.google.common.base.Preconditions.checkArgument;
 
 import java.util.Optional;
-
-import static com.google.common.base.Preconditions.checkArgument;
 
 /**
  * Created by Dimitry on 14.05.2016.
  */
-public class Board {
-    private final int numberOfRows;
-    private final int numberOfColumns;
-    private Player[][] occupiers;
+public class Board
+{
+  private Player startingPlayer;
+  private final int numberOfRows;
+  private final int numberOfColumns;
+  private Player[][] occupiers;
 
-    public Board(int numberOfRows, int numberOfColumns) {
-        this.numberOfRows = numberOfRows;
-        this.numberOfColumns = numberOfColumns;
-        occupiers = new Player[numberOfRows][numberOfColumns];
-    }
+  public Board(final int numberOfRows, final int numberOfColumns) {
+    this.numberOfRows = numberOfRows;
+    this.numberOfColumns = numberOfColumns;
+    occupiers = new Player[numberOfRows][numberOfColumns];
+  }
 
-    public Optional<Player> slotOwner(int row, int column) {
-        if(occupiers[row][column] == null) {
-            return Optional.empty();
-        } else {
-            return Optional.of(occupiers[row][column]);
-        }
+  public Optional<Player> slotOwner(final int row, final int column) {
+    if (occupiers[row][column] == null) {
+      return Optional.empty();
+    } else {
+      return Optional.of(occupiers[row][column]);
     }
+  }
 
-    public void insertChip(int columnNumber, Player player) {
-        int index = numberOfRows - 1;
-        while(index >= 0 && occupiers[index][columnNumber] != null) {
-            index--;
-        }
-        checkArgument(index >= 0);
-        occupiers[index][columnNumber] = player;
+  public void insertChip(final int columnNumber, final Player player) {
+    int index = numberOfRows - 1;
+    while (index >= 0 && occupiers[index][columnNumber] != null) {
+      index--;
     }
+    checkArgument(index >= 0);
+    occupiers[index][columnNumber] = player;
+  }
 
-    public int getNumberOfRows() {
-        return numberOfRows;
-    }
+  public int getNumberOfRows() {
+    return numberOfRows;
+  }
 
-    public int getNumberOfColumns() {
-        return numberOfColumns;
-    }
+  public int getNumberOfColumns() {
+    return numberOfColumns;
+  }
+
+  public void setStartingPlayer(final Player player) {
+    this.startingPlayer = player;
+  }
+
+  public Player getStartingPlayer() {
+    return startingPlayer;
+  }
 }
