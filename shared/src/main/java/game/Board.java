a46066072759f897df7d87a15a376a00b212d7f6
goeckeler <thorsten@goeckeler.com>
2016-05-14 17:19:33 +0200

    Added canInsert()

--- shared/src/main/java/game/Board.java
+++ shared/src/main/java/game/Board.java
@@ -1,9 +1,5 @@
 package game;
 
-import com.google.common.base.Preconditions;
-
-import java.util.Optional;
-
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.util.Optional;
@@ -13,56 +9,54 @@ import java.util.Optional;
  */
 public class Board
 {
-    private final int numberOfRows;
-    private final int numberOfColumns;
-    private Player[][] occupiers;
+  private final int numberOfRows;
+  private final int numberOfColumns;
+  private Player[][] occupiers;
 
-    public Board(final int numberOfRows, final int numberOfColumns) {
-        this.numberOfRows = numberOfRows;
-        this.numberOfColumns = numberOfColumns;
-        occupiers = new Player[numberOfRows][numberOfColumns];
-    }
+  public Board(final int numberOfRows, final int numberOfColumns) {
+    this.numberOfRows = numberOfRows;
+    this.numberOfColumns = numberOfColumns;
+    occupiers = new Player[numberOfRows][numberOfColumns];
+  }
 
-    public Optional<Player> slotOwner(final int row, final int column) {
-        if (occupiers[row][column] == null) {
-            return Optional.empty();
-        } else {
-            return Optional.of(occupiers[row][column]);
-        }
+  public Optional<Player> slotOwner(final int row, final int column) {
+    if (occupiers[row][column] == null) {
+      return Optional.empty();
+    } else {
+      return Optional.of(occupiers[row][column]);
     }
+  }
 
-    public void insertChip(int columnNumber, Player player) {
-        int index = lastUnoccupiedRowInColumn(columnNumber);
-        checkArgument(index >= 0);
-        occupiers[index][columnNumber] = player;
-    }
+  public void insertChip(final int columnNumber, final Player player) {
+    final int index = lastUnoccupiedRowInColumn(columnNumber);
+    checkArgument(index >= 0);
+    occupiers[index][columnNumber] = player;
+  }
 
-    public int lastUnoccupiedRowInColumn(int columnNumber) {
-        int index = numberOfRows - 1;
-        while(index >= 0 && occupiers[index][columnNumber] != null) {
-            index--;
-        }
-        return index;
+  public int lastUnoccupiedRowInColumn(final int columnNumber) {
+    int index = numberOfRows - 1;
+    while (index >= 0 && occupiers[index][columnNumber] != null) {
+      index--;
     }
+    return index;
+  }
 
-    public boolean canInsert(int columnNumber) {
-      return lastUnoccupiedRowInColumn(columnNumber) >= 0;
-    }
-    
-    public int getNumberOfRows() {
-        return numberOfRows;
-    }
+  public boolean canInsert(final int columnNumber) {
+    return lastUnoccupiedRowInColumn(columnNumber) >= 0;
+  }
 
-    public int getNumberOfColumns() {
-        return numberOfColumns;
-    }
+  public int getNumberOfRows() {
+    return numberOfRows;
+  }
 
-    public boolean isBoardFull() {
-        for (int i = 0; i < numberOfColumns; ++i) {
-            if (lastUnoccupiedRowInColumn(i) >= 0) {
-                return false;
-            }
-        }
-        return true;
+  public int getNumberOfColumns() {
+    return numberOfColumns;
+  }
+
+  public boolean isBoardFull() {
+    for (int i = 0; i < numberOfColumns; ++i) {
+      if (lastUnoccupiedRowInColumn(i) >= 0) { return false; }
     }
+    return true;
+  }
 }
