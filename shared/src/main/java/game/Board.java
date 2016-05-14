ee560a961204e15d3db336d45cc2f9893b63330a
Dimitry Polivaev <dpolivaev@gmx.de>
2016-05-14 11:50:00 +0200

    boardIsEmptyAtTheBeginning

--- shared/src/main/java/game/Board.java
+++ shared/src/main/java/game/Board.java
@@ -1,7 +1,12 @@
 package game;
 
+import java.util.Optional;
+
 /**
  * Created by Dimitry on 14.05.2016.
  */
 public class Board {
+    public Optional<Player> slotOwner(int row, int column) {
+        return Optional.empty();
+    }
 }
--- shared/src/test/java/game/BoardTest.java
+++ shared/src/test/java/game/BoardTest.java
@@ -1,13 +1,24 @@
 package game;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by Dimitry on 14.05.2016.
  */
 public class BoardTest {
     @Test
-    public void test() {
+    public void boardIsEmptyAtTheBeginning() {
         Board board = new Board();
+        int row = 0;
+        int column = 0;
+
+        Optional<Player> owner = board.slotOwner(row, column);
+
+        assertThat(owner).isEmpty();
     }
 }
