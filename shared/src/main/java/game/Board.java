bd420ca78753217741e014d3d66bf620f1ee2b81
Dimitry Polivaev <dpolivaev@gmx.de>
2016-05-14 12:00:15 +0200

    boardShouldHaveCorrectDimensions

--- shared/src/main/java/game/Board.java
+++ shared/src/main/java/game/Board.java
@@ -6,7 +6,27 @@ import java.util.Optional;
  * Created by Dimitry on 14.05.2016.
  */
 public class Board {
+    private int numberOfRows;
+    private int numberOfColumns;
+
+    public Board(int numberOfRows, int numberOfColumns) {
+        this.numberOfRows = numberOfRows;
+        this.numberOfColumns = numberOfColumns;
+    }
+
     public Optional<Player> slotOwner(int row, int column) {
         return Optional.empty();
+    }
+
+    public void insertChip(int columnNumber, Player player) {
+
+    }
+
+    public int getNumberOfRows() {
+        return numberOfRows;
+    }
+
+    public int getNumberOfColumns() {
+        return numberOfColumns;
     }
 }
--- shared/src/test/java/game/BoardTest.java
+++ shared/src/test/java/game/BoardTest.java
@@ -1,24 +1,41 @@
 package game;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by Dimitry on 14.05.2016.
  */
 public class BoardTest {
+
+    private Board board = new Board(6, 7);
+
+    @Test
+    public void boardShouldHaveCorrectDimensions() {
+        assertThat(board.getNumberOfRows()).isEqualTo(6);
+        assertThat(board.getNumberOfColumns()).isEqualTo(7);
+    }
+
     @Test
     public void boardIsEmptyAtTheBeginning() {
-        Board board = new Board();
         int row = 0;
         int column = 0;
 
         Optional<Player> owner = board.slotOwner(row, column);
 
         assertThat(owner).isEmpty();
+    }
+
+    @Test
+    public void slotShouldBeOccupiedAfterPlacing() {
+        Player player = mock(Player.class);
+
+        board.insertChip(0, player);
+
+        board.slotOwner(5, 0);
     }
 }
