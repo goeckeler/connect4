094b0f0309b9fee1c6d113d7a27ec22a816ad0e9
Dimitry Polivaev <dpolivaev@gmx.de>
2016-05-14 11:29:40 +0200

    board

---
+++ src/main/java/game/Board.java
@@ -0,0 +1,7 @@
+package game;
+
+/**
+ * Created by Dimitry on 14.05.2016.
+ */
+public class Board {
+}
---
+++ src/test/java/game/BoardTest.java
@@ -0,0 +1,13 @@
+package game;
+
+import org.junit.Test;
+
+/**
+ * Created by Dimitry on 14.05.2016.
+ */
+public class BoardTest {
+    @Test
+    public void test() {
+        Board board = new Board();
+    }
+}
